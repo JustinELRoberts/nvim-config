c6eb95a87b4ee9113afce1828ce594b32683cb96
Justin Roberts <JustinEdwardLeo@gmail.com>
2024-11-23 19:35:22 -0500

    Initial commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+-- This plugin provides overall theming
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
+  end
+}
---
+++ lua/vim-settings.lua
@@ -0,0 +1,10 @@
+-- Set tab to 2 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader=" "
+vim.g.maplocalleader = "\\"
+
+vim.opt.scrolloff = 10
