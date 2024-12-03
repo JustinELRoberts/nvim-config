8d1ca60944939e142731ce80513f43e2e9fbc0d5
Justin Roberts <jroberts329@bloomberg.net>
2024-12-03 15:41:18 -0500

    Improved rust LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,6 +30,22 @@ return {
       lspconfig.html.setup({
         capabilities = capabilities
       })
+      lspconfig.rust_analyzer.setup {
+        filetypes = {"rust"},
+        on_attach = function(client, bufnr)
+          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end,
+        settings = {
+          ['rust_analyzer'] = {
+            cargo = {
+              allFeatures = true
+            },
+            checkOnSave = {
+              command = "clippy"
+            },
+          },
+        },
+      }
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
