7d9069a5fdc6466f2f0ef179be162f29c0618293
Justin Roberts <jroberts329@bloomberg.net>
2024-12-03 16:14:47 -0500

    Fixed keybinds and added ripgrep support

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,11 +6,12 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    "BurntSushi/ripgrep",
   },
   config = function()
     vim.keymap.set('n', '<leader>fe', ':Neotree focus filesystem left<CR>')
-    vim.keymap.set('n', '<leader>fb', ':Neotree focus buffers left<CR>')
-    vim.keymap.set('n', '<leader>fg', ':Neotree focus git_status left<CR>')
+    vim.keymap.set('n', '<leader>be', ':Neotree focus buffers left<CR>')
+    vim.keymap.set('n', '<leader>ge', ':Neotree focus git_status left<CR>')
     vim.keymap.set('n', '<leader>ce', ':Neotree close<CR>')
 
     require("neo-tree").setup({
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,9 @@ return  {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope grep' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     end
   },
   -- This plugin gives our code actions (from lsp-config.lua) a nice modal popup
