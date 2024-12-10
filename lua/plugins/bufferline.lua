39ff8752727cfa3e51d05454f09c2db95dd3c371
Justin Roberts <jroberts329@bloomberg.net>
2024-12-10 10:28:46 -0500

    Added bufferline and some keybinds to navigate between them

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local bufferline = require('bufferline')
+    bufferline.setup({
+      options = {
+        separator_style = 'slant',
+        buffer_close_icon = '',
+      }
+    })
+    -- This isn't a bufferline specific keybind, but switching buffers is sufficiently related that it feels like a good place
+    vim.keymap.set('n', '[b', ':bp<CR>')
+    vim.keymap.set('n', ']b', ':bn<CR>')
+  end
+}
