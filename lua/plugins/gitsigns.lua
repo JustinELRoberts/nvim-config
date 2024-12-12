fc6260cc857f0ba73575e8dc432e39ba35ba6c8f
Justin Roberts <jroberts329@bloomberg.net>
2024-12-12 12:37:24 -0500

    Added git keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,30 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require('gitsigns').setup({
+    local gitsigns = require('gitsigns')
+
+    gitsigns.setup({
       current_line_blame = true,
     })
+
+    -- If we are in visual mode, we will be staging only the hunk in our selection
+    vim.keymap.set("v", "<leader>gs", gitsigns.stage_hunk, {})
+
+    -- If we are in normal mode, we will be staging/unstaging the entire buffer
+    vim.keymap.set("n", "<leader>gs", gitsigns.stage_buffer, {})
+    vim.keymap.set("n", "<leader>gr", gitsigns.reset_buffer_index, {})
+
+    -- Undo the last staged hunk
+    vim.keymap.set("n", "<leader>gu", gitsigns.undo_stage_hunk, {})
+
+    -- Select the hunk we are inside of 
+    vim.keymap.set("n", "<leader>gh", gitsigns.select_hunk, {})
+
+    -- Open a git blame buffer
+    vim.keymap.set("n", "<leader>gb", gitsigns.blame, {})
+
+    -- Next and previous git hunk
+    vim.keymap.set("n", "[g", gitsigns.next_hunk, {})
+    vim.keymap.set("n", "]g", gitsigns.prev_hunk, {})
   end
 }
