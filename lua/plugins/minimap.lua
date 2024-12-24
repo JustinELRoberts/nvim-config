8ae6cafe8d172ca6c9a8dc17c512690a014d9aba
Justin Roberts <JustinEdwardLeo@gmail.com>
2024-12-23 21:02:52 -0500

    Moved to a new minimap plugin

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,12 +1,22 @@
 return {
-  'echasnovski/mini.map',
-  version = '*',
-  config = function()
-    local minimap = require('mini.map')
-    minimap.setup()
-    minimap.toggle()
-    vim.keymap.set('n', '<Leader>mt', minimap.toggle)
-    vim.keymap.set('n', '<Leader>mf', minimap.toggle_focus)
-    vim.keymap.set('n', '<Leader>mr', minimap.refresh)
-  end
+  "Isrothy/neominimap.nvim",
+  version = "v3.*.*",
+  lazy = false,
+  keys = {
+    -- Global Minimap Controls
+    { "<leader>mt",  "<cmd>Neominimap toggle<cr>",      desc = "Toggle global minimap" },
+    { "<leader>mr",  "<cmd>Neominimap refresh<cr>",     desc = "Refresh global minimap" },
+
+    -- Buffer-Specific Minimap Controls
+    { "<leader>mbt", "<cmd>Neominimap bufToggle<cr>",   desc = "Toggle minimap for current buffer" },
+    { "<leader>mbr", "<cmd>Neominimap bufRefresh<cr>",  desc = "Refresh minimap for current buffer" },
+
+    ---Focus Controls
+    { "<leader>mf",  "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+  },
+  init = function()
+    vim.g.neominimap = {
+      layout = "split",
+    }
+  end,
 }
