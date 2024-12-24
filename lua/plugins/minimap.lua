3ef06eed13fada0627c8191db82b867e1eaa83fb
Justin Roberts <JustinEdwardLeo@gmail.com>
2024-12-24 09:43:27 -0500

    Hide notifications for minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,22 +1,23 @@
 return {
-  "Isrothy/neominimap.nvim",
-  version = "v3.*.*",
-  lazy = false,
-  keys = {
-    -- Global Minimap Controls
-    { "<leader>mt",  "<cmd>Neominimap toggle<cr>",      desc = "Toggle global minimap" },
-    { "<leader>mr",  "<cmd>Neominimap refresh<cr>",     desc = "Refresh global minimap" },
+	"Isrothy/neominimap.nvim",
+	version = "v3.*.*",
+	lazy = false,
+	keys = {
+		-- Global Minimap Controls
+		{ "<leader>mt", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
+		{ "<leader>mr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
 
-    -- Buffer-Specific Minimap Controls
-    { "<leader>mbt", "<cmd>Neominimap bufToggle<cr>",   desc = "Toggle minimap for current buffer" },
-    { "<leader>mbr", "<cmd>Neominimap bufRefresh<cr>",  desc = "Refresh minimap for current buffer" },
+		-- Buffer-Specific Minimap Controls
+		{ "<leader>mbt", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
+		{ "<leader>mbr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
 
-    ---Focus Controls
-    { "<leader>mf",  "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
-  },
-  init = function()
-    vim.g.neominimap = {
-      layout = "split",
-    }
-  end,
+		---Focus Controls
+		{ "<leader>mf", "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+	},
+	init = function()
+		vim.g.neominimap = {
+			layout = "split",
+			notification_level = vim.log.levels.OFF,
+		}
+	end,
 }
