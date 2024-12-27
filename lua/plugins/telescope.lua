ae382bbcb4b9391d97602dde6a823147b8bc8968
Justin Roberts <jroberts329@bloomberg.net>
2024-12-27 16:23:20 -0500

    Improved telescope config, adding custom multigrep logic

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,62 @@
-return  {
-  -- This plugin is used to find files 
+return {
+  -- This plugin is used to find files
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+
+      -- Search working directory
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Search file names' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search active buffer contents' })
+      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Find references to item under cursor' })
+      -- Below is a custom finder found in ../telescope/multigrep.lua
+      require "telescope.multigrep".setup()
+
+      -- Search nvim config, packages, etc
+      vim.keymap.set('n', '<leader>fv', function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config")
+        })
+      end, { desc = 'Search neovim config file names' })
+
+      vim.keymap.set('n', '<leader>fp', function()
+        builtin.find_files({
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        })
+      end, { desc = 'Search every file that is installed in a plugin' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Search help tags' })
     end
   },
+
   -- This plugin gives our code actions (from lsp-config.lua) a nice modal popup
   {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
-      require("telescope").setup {
+      local telescope = require('telescope')
+
+      telescope.setup {
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {
             }
-          }
+          },
+          fzf = {},
         },
         defaults = {
           layout_strategy = "vertical",
           layout_config = {
             vertical = { width = 0.8 }
           }
-        }
+        },
       }
-      require("telescope").load_extension("ui-select")
+
+      telescope.load_extension("ui-select")
+      telescope.load_extension("fzf")
     end
   }
 }
