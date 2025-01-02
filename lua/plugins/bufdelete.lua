87a087a02aca05adf4fbadbce0b147b87735bdf5
Justin Roberts <jroberts329@bloomberg.net>
2025-01-02 16:38:07 -0500

    Fixed keybind

--- lua/plugins/bufdelete.lua
+++ lua/plugins/bufdelete.lua
@@ -6,7 +6,11 @@ return {
     vim.keymap.set('n', '<leader>bd', bufdelete.bufwipeout)
 
     -- These aren't specific to bufdelete but I will keep them here nevertheless
-    vim.keymap.set('n', '[b', ':bp<CR>')
-    vim.keymap.set('n', ']b', ':bn<CR>')
+    vim.keymap.set('n', '[b', function()
+      return vim.v.count .. ":bprev<CR>"
+    end, { expr = true })
+    vim.keymap.set('n', ']b', function()
+      return vim.v.count .. ":bnext<CR>"
+    end, { expr = true })
   end
 }
