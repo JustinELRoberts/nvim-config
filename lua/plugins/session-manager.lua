400f495f9f9ff3c812869996f41357787daebd8a
Justin Roberts <jroberts329@bloomberg.net>
2025-01-11 23:09:12 -0500

    sessions now are based on git repo; not dir

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -6,7 +6,7 @@ return {
     local session_manager = require('session_manager')
     session_manager.setup({
       sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'),  -- The directory where the session files will be saved.
-      autoload_mode = config.AutoloadMode.CurrentDir,               -- Define what to do when Neovim is started without arguments. See "Autoload mode" section below.
+      autoload_mode = config.AutoloadMode.GitSession,               -- Define what to do when Neovim is started without arguments. See "Autoload mode" section below.
     })
 
     vim.keymap.set('n', '<leader>sl', session_manager.load_session)
