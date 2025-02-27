7563db691fb090e9d7a2b918816316d48b9df2cc
Justin Roberts <jroberts329@bloomberg.net>
2025-02-27 11:56:38 -0500

    Removed hardtime

--- lua/plugins/hardtime.lua
+++
@@ -1,6 +0,0 @@
--- This package prevents me from using the arrow keys, holding hjkl, etc to promote better motions
-return {
-  "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {}
-}
