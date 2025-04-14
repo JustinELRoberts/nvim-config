4eb5e71e899c9742ae65f2df01d69a699ea1411a
Justin Roberts <JustinEdwardLeo@gmail.com>
2025-04-14 19:07:39 -0400

    Custom logging macros

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -31,24 +31,40 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local lspformat = require("lsp-format")
+      local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
 
       lspconfig.lua_ls.setup({
-        on_attach = lspformat.on_attach
+        on_attach = function(client, bufnr)
+          lspformat.on_attach(client, bufnr)
+          -- Custom macro to print() the active selection
+          vim.fn.setreg("l", "yoprint('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc .. "")
+        end
       })
 
       lspconfig.ts_ls.setup({
-        on_attach = lspformat.on_attach
+        on_attach = function(client, bufnr)
+          lspformat.on_attach(client, bufnr)
+          -- Custom macro to console.log() the active selection
+          vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc .. "")
+        end
       })
 
       lspconfig.html.setup({
-        on_attach = lspformat.on_attach
+        on_attach = function(client, bufnr)
+          lspformat.on_attach(client, bufnr)
+          -- Custom macro to console.log() the active selection
+          vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc .. "")
+        end
       })
 
       lspconfig.rust_analyzer.setup {
         filetypes = { "rust" },
         on_attach = function(client, bufnr)
-          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
           lspformat.on_attach(client, bufnr)
+          -- Enable type hints
+          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          -- Custom macro to println!() the active selection
+          vim.fn.setreg("l", "yoprintln!(\"" .. esc .. "pa: {:#?}\", " .. esc .. "pa);" .. esc .. "")
         end,
         settings = {
           ['rust_analyzer'] = {
@@ -64,6 +80,10 @@ return {
       }
 
       lspconfig.pyright.setup({
+        on_attach = function(_, _)
+          -- Custom macro to print() the active selection
+          vim.fn.setreg("l", "yoprint('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc .. "")
+        end,
         settings = {
           python = {
             pythonPath = vim.fn.exepath("python3"),
